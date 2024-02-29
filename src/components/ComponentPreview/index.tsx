3fbf737d7ccf0503f8f607aba9cf3193ef4f880b
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-29 19:18:00 +0900

    feat: プレビュー表示のコンポーネントを外部化

---
+++ src/components/ComponentPreview/index.tsx
@@ -0,0 +1,37 @@
+import * as YamadaUI from "@yamada-ui/react";
+import { Box, Text } from "@yamada-ui/react";
+import React, { FC } from "react";
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
+
+export const ComponentPreview:FC<{path: string; code: string;}> = ({path, code}) => {
+    const transformCode = (code: string) => {
+        const codeLines = code.split('\n');
+        let codeSnippet = ''; // React Liveで表示させるコード
+        let componentName = ''; // コンポーネント名
+    
+        for (const line of codeLines) {
+            // import {} from "" を削除
+            if (!line.includes('import')) {
+                codeSnippet += line + '\n';
+                if (codeSnippet.includes('const')) {
+                    // const 変数名をcomponentNameに格納
+                    const match = line.match(/const\s+(\w+)\s*:\s*FC\s*=\s*\(\s*\)\s*=>\s*{/);
+                    if (match && match.length >= 2) {
+                        componentName = match[1];
+                    }
+                }
+            }
+        }
+        return `${codeSnippet}\nrender(<${componentName}/>)` 
+    }
+    return <Box>
+    <Text>Component: {path}</Text>
+    <LiveProvider code={code} scope={{ ...YamadaUI, ...React }} enableTypeScript noInline transformCode={transformCode} >
+        <Box>
+            <LiveEditor />
+            <LivePreview />
+        </Box>
+        <LiveError />
+    </LiveProvider>
+</Box>
+}
--- src/pages/component/[component].tsx
+++ src/pages/component/[component].tsx
@@ -6,6 +6,7 @@ import { ParsedUrlQuery } from "querystring";
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
 import React from "react"
 import { getAllComponents } from "@/data/components";
+import { ComponentPreview } from "@/components/ComponentPreview";
 const { Box, Container, Text } = YamadaUI
 
 interface PageProps {
@@ -50,37 +51,9 @@ export const getStaticProps: GetStaticProps<PageProps, PageParams> = async ({ pa
 }
 
 const Page: NextPage<PageProps> = ({ data }) => {
-    const transformCode = (code: string) => {
-        const codeLines = code.split('\n');
-        let codeSnippet = ''; // React Liveで表示させるコード
-        let componentName = ''; // コンポーネント名
-
-        for (const line of codeLines) {
-            // import {} from "" を削除
-            if (!line.includes('import')) {
-                codeSnippet += line + '\n';
-                if (codeSnippet.includes('const')) {
-                    // const 変数名をcomponentNameに格納
-                    const match = line.match(/const\s+(\w+)\s*:\s*FC\s*=\s*\(\s*\)\s*=>\s*{/);
-                    if (match && match.length >= 2) {
-                        componentName = match[1];
-                    }
-                }
-            }
-        }
-        return `${codeSnippet}\nrender(<${componentName}/>)` 
-    }
+    
     return <Container>
-        <Box>
-            <Text>Component: {data.path}</Text>
-            <LiveProvider code={data.component} scope={{ ...YamadaUI, ...React }} enableTypeScript noInline transformCode={transformCode} >
-                <Box>
-                    <LiveEditor />
-                    <LivePreview />
-                </Box>
-                <LiveError />
-            </LiveProvider>
-        </Box>
+        <ComponentPreview path={data.path} code={data.component}/>
     </Container>;
 }
 
