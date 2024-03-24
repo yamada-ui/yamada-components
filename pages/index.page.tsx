f7733f3d1d092446e66a22775953d67f806b82aa
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-24 16:29:10 +0900

    fix: fixed PageProps

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,15 +1,22 @@
 import { Container, Heading, VStack, Wrap } from "@yamada-ui/react"
-import type { GetStaticProps, NextPage } from "next"
+import type { InferGetStaticPropsType, NextPage } from "next"
 import { CategoryCard } from "components/layouts/category-card"
 import { useI18n } from "contexts/i18n-context"
 import { CATEGORIES } from "data/categories"
 import type { CategoriesGroup } from "data/types"
 import { AppLayout } from "layouts/app-layout"
 
-// type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>
-type PageProps = {
-  data: {
-    list: CategoriesGroup[]
+type PageProps = InferGetStaticPropsType<typeof getStaticProps>
+
+export const getStaticProps = async () => {
+  const list: CategoriesGroup[] = CATEGORIES
+  const data = {
+    list,
+  }
+  return {
+    props: {
+      data,
+    },
   }
 }
 
@@ -39,17 +46,3 @@ const Page: NextPage<PageProps> = ({ data }) => {
 }
 
 export default Page
-
-export const getStaticProps: GetStaticProps<PageProps> = async () => {
-  const list: CategoriesGroup[] = CATEGORIES
-  const data: PageProps["data"] = {
-    list,
-  }
-  return {
-    props: {
-      data,
-    },
-  } as { props: PageProps }
-}
-
-// export const getServerSideProps = getServerSideCommonProps
