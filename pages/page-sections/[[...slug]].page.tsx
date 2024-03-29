b217bffcda74ac7834cb51eb9fed7d9421c0b4df
yuto ichinose <yuto.0526.sano@gmail.com>
2024-03-30 01:36:14 +0900

    feat: common `getStaticDocumentProps`

--- pages/page-sections/[[...slug]].page.tsx
+++ pages/page-sections/[[...slug]].page.tsx
@@ -1,53 +1,15 @@
-import { readFileSync } from "fs"
-import path from "path"
 import { Container } from "@yamada-ui/react"
 import type { InferGetStaticPropsType, NextPage } from "next"
 import { ComponentPreview } from "components/layouts"
 import { useI18n } from "contexts/i18n-context"
 import { AppLayout } from "layouts/app-layout"
-import { getStaticDocumentPaths } from "utils/next"
+import { getStaticDocumentPaths, getStaticDocumentProps } from "utils/next"
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>
 
 export const getStaticPaths = getStaticDocumentPaths("page-sections")
 
-export const getStaticProps = async ({
-  params,
-}: {
-  params: {
-    slug: string[]
-  }
-}) => {
-  const component = params.slug.join("/")
-
-  const filePath = path.join(
-    process.cwd(),
-    "contents",
-    "page-sections",
-    component.toLowerCase(),
-    "index.tsx",
-  )
-
-  const fileContent = readFileSync(filePath, "utf8")
-  const index = fileContent
-    .split("\n")
-    .findIndex((v) => /export\s+const\s+metadata\s*=\s*{/.test(v))
-
-  const data = {
-    path: component.split("/")[1],
-    component: fileContent
-      .split("\n")
-      .slice(0, index)
-      .filter((line) => !line.includes("export"))
-      .join("\n"),
-  }
-
-  return {
-    props: {
-      data,
-    },
-  }
-}
+export const getStaticProps = getStaticDocumentProps("page-sections")
 
 const Page: NextPage<PageProps> = ({ data }) => {
   const { t } = useI18n()
--- utils/next.ts
+++ utils/next.ts
@@ -1,4 +1,10 @@
-import type { GetServerSidePropsContext, GetStaticPathsContext } from "next"
+import { readFileSync } from "fs"
+import path from "path"
+import type {
+  GetServerSidePropsContext,
+  GetStaticPathsContext,
+  GetStaticPropsContext,
+} from "next"
 import { getPaths } from "./contentlayer"
 
 export const getServerSideCommonProps = async ({
@@ -15,4 +21,38 @@ export const getStaticDocumentPaths =
     const paths = getPaths(documentTypeName)
 
     return { paths, fallback: false }
+  }
+
+export const getStaticDocumentProps =
+  (documentTypeName: string) =>
+  async ({ params }: GetStaticPropsContext) => {
+    const component = (params.slug as string[]).join("/")
+
+    const filePath = path.join(
+      process.cwd(),
+      "contents",
+      documentTypeName,
+      component.toLowerCase(),
+      "index.tsx",
+    )
+
+    const fileContent = readFileSync(filePath, "utf8")
+    const index = fileContent
+      .split("\n")
+      .findIndex((v) => /export\s+const\s+metadata\s*=\s*{/.test(v))
+
+    const data = {
+      path: component.split("/")[1],
+      component: fileContent
+        .split("\n")
+        .slice(0, index)
+        .filter((line) => !line.includes("export"))
+        .join("\n"),
+    }
+
+    return {
+      props: {
+        data,
+      },
+    }
   }
