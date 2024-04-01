f083263e323a0a6644c931ca40ccee8799c3c92a
yuto ichinose <yuto.0526.sano@gmail.com>
2024-04-02 07:08:52 +0900

    fix: remove `getComponentsByCategory` in `categoryts`

--- data/categories.ts
+++ data/categories.ts
@@ -1,7 +1,5 @@
-import { readFileSync } from "fs"
 import path from "path"
 import type { CategoriesGroup } from "types"
-import { toKebabCase } from "utils/assetion"
 import { getDirNames } from "utils/component"
 // import images from './images';
 
@@ -22,34 +20,3 @@ export const CATEGORIES_SLUGS = ALL_CATEGORIES.map((item) => item.slug)
 
 export const getCategoryData = (category: string) =>
   ALL_CATEGORIES.find((item) => item.slug === category)
-
-export const getComponentsByCategory = async (category: string) => {
-  const root = path.join(contentsDir, category)
-  const components = getDirNames(root)
-  const promises = components.map(async (componentName: string) => {
-    const { metadata } = await import(
-      `../contents/${category}/${componentName}/index`
-    )
-
-    const filePath = path.join(root, componentName, "index.tsx")
-
-    const fileContent = readFileSync(filePath, "utf8")
-    const index = fileContent
-      .split("\n")
-      .findIndex((v) => /export\s+const\s+metadata\s*=\s*{/.test(v))
-    const code = fileContent
-      .split("\n")
-      .slice(0, index)
-      .filter((line) => !line.includes("export"))
-      .join("\n")
-
-    return {
-      component: `${category}/${componentName}`,
-      slug: toKebabCase(`${category}/${componentName}`),
-      attributes: metadata,
-      code: code,
-    }
-  })
-
-  return await Promise.all(promises)
-}
