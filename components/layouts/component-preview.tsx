1ac6e12bcf45831c04a46a9fd6458edf7e656d1a
yuto ichinose <yuto.0526.sano@gmail.com>
2024-04-22 21:12:33 +0900

    feat: theme import

--- components/layouts/component-preview.tsx
+++ components/layouts/component-preview.tsx
@@ -4,12 +4,13 @@ import {
   Tab,
   TabPanel,
   Tabs,
+  UIProvider,
   useBoolean,
 } from "@yamada-ui/react"
 import type { SkeletonProps } from "@yamada-ui/react"
 import dynamic from "next/dynamic"
 import type { ComponentProps, FC } from "react"
-import React, { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { DividedComponent } from "./devided-component"
 import { useI18n } from "contexts/i18n-context"
 
@@ -26,29 +27,45 @@ export const ComponentPreview: FC<ComponentPreviewProps> = ({
   // ダイナミックインポート
   const Component = dynamic(() => import(`../../contents/${path}`))
 
+  const [uiTheme, setUiTheme] = useState()
+
+  const loadComponent = async () => {
+    const { theme } = await import(`../../contents/${path}`)
+    setUiTheme(theme)
+  }
+
+  useEffect(() => {
+    loadComponent()
+    /* eslint-disable-next-line */
+  }, [])
+
+  console.log(uiTheme)
+
   return (
-    <Box my="6">
-      <Tabs align="end">
-        <Tab>{t("component.component-preview.tab.preview")}</Tab>
-        <Tab>{t("component.component-preview.tab.code")}</Tab>
-        <TabPanel mt={10}>
-          <Preview>
-            <Box
-              as={Component}
-              p="md"
-              borderWidth="1px"
-              rounded="md"
-              overflowX="auto"
-            />
-          </Preview>
-        </TabPanel>
-        <TabPanel>
-          <Box rounded="md" overflow="hidden" my="4" position="relative">
-            <DividedComponent component={component} />
-          </Box>
-        </TabPanel>
-      </Tabs>
-    </Box>
+    <UIProvider {...(uiTheme ? { theme: uiTheme } : {})}>
+      <Box my="6">
+        <Tabs align="end">
+          <Tab>{t("component.component-preview.tab.preview")}</Tab>
+          <Tab>{t("component.component-preview.tab.code")}</Tab>
+          <TabPanel mt={10}>
+            <Preview>
+              <Box
+                as={Component}
+                p="md"
+                borderWidth="1px"
+                rounded="md"
+                overflowX="auto"
+              />
+            </Preview>
+          </TabPanel>
+          <TabPanel>
+            <Box rounded="md" overflow="hidden" my="4" position="relative">
+              <DividedComponent component={component} />
+            </Box>
+          </TabPanel>
+        </Tabs>
+      </Box>
+    </UIProvider>
   )
 }
 
