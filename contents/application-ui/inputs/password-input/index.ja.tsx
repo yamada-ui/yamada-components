3e25ff2b1b9e03647ce9e4f050869088395ef42f
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2024-04-29 16:00:42 +0900

    feat(inputs): add password-input ja version

---
+++ contents/application-ui/inputs/password-input/index.ja.tsx
@@ -0,0 +1,34 @@
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useBoolean,
+} from "@yamada-ui/react"
+import type { FC } from "react"
+import type { ComponentMetadata } from "types"
+
+const PasswordInput: FC = () => {
+  const [show, { toggle }] = useBoolean()
+  return (
+    <InputGroup size="md">
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        placeholder="パスワード"
+      />
+      <InputRightElement w="4.5rem" isClick>
+        <Button h="1.75rem" size="sm" onClick={toggle}>
+          {show ? "隠す" : "表示"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  )
+}
+
+export default PasswordInput
+
+export const metadata: ComponentMetadata = {
+  title: "パスワード入力",
+  description: "パスワードを入力するコンポーネント",
+}
