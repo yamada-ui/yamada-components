f1e08cc23eb681a8b872d25e4a7e53e415808889
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-10 23:37:01 +0900

    refactor(component-layout): updated props and state management

--- layouts/component-layout.tsx
+++ layouts/component-layout.tsx
@@ -1,20 +1,32 @@
-import type { FC, PropsWithChildren } from "react"
+import type { ResizableProps } from "@yamada-ui/react"
+import { useDisclosure } from "@yamada-ui/react"
+import { useState, type FC } from "react"
+import { ComponentBody, ComponentHeader } from "components/layouts"
 import { SEO } from "components/media-and-icons"
 
-type ComponentLayoutOptions = { title: string; description: string }
+export type CodeDirection = ResizableProps["direction"]
 
-export type ComponentLayoutProps = PropsWithChildren & ComponentLayoutOptions
+type ComponentLayoutOptions = {
+  title: string
+  description: string
+}
+
+export type ComponentLayoutProps = ComponentLayoutOptions
 
 export const ComponentLayout: FC<ComponentLayoutProps> = ({
   title,
   description,
-  children,
 }) => {
+  const codeControls = useDisclosure()
+  const [codeDirection, setCodeDirection] = useState<CodeDirection>("vertical")
+
   return (
     <>
       <SEO title={title} description={description} />
 
-      {children}
+      <ComponentHeader {...{ codeDirection, codeControls }} />
+
+      <ComponentBody {...{ codeDirection, setCodeDirection, codeControls }} />
     </>
   )
 }
