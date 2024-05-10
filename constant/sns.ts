aa0181d1b585b8de4b587f62aaf7311097258f5c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-10 23:33:15 +0900

    feat(constant/sns): added edit URL for yamada components

--- constant/sns.ts
+++ constant/sns.ts
@@ -3,6 +3,8 @@ export const SNS = {
     HIROTOMO_YAMADA: "https://github.com/hirotomoyamada",
     YAMADA_UI: "https://github.com/yamada-ui/yamada-ui",
     YAMADA_COMPONENTS: "https://github.com/yamada-ui/yamada-components",
+    EDIT_URL:
+      "https://github.com/yamada-ui/yamada-components/tree/main/contents",
   },
   TWITTER: {
     HIROTOMO_YAMADA: "https://twitter.com/hirotomoyamada",
