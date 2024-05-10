62a6410e0980f0997a4cc6bd22c3db00665b33fa
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-10 23:36:43 +0900

    feat(component-context): added new context for components

---
+++ contexts/component-context.tsx
@@ -0,0 +1,28 @@
+import { createContext, useMemo, useContext } from "react"
+import type { PropsWithChildren, FC } from "react"
+import type { Component } from "utils/component"
+
+type ComponentContext = Component
+
+const ComponentContext = createContext<ComponentContext>({} as Component)
+
+export type ComponentProviderProps = PropsWithChildren<Component>
+
+export const ComponentProvider: FC<ComponentProviderProps> = ({
+  children,
+  ...component
+}) => {
+  const value = useMemo(() => component, [component])
+
+  return (
+    <ComponentContext.Provider value={value}>
+      {children}
+    </ComponentContext.Provider>
+  )
+}
+
+export const useComponent = () => {
+  const context = useContext(ComponentContext)
+
+  return context
+}
