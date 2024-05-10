99d2d5f1e13082747d703bb28f8047ef6f50c5e0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-10 23:36:20 +0900

    feat(app-context): expanded AppContext with new properties

--- contexts/app-context.tsx
+++ contexts/app-context.tsx
@@ -1,14 +1,33 @@
 import { createContext, useMemo, useContext } from "react"
 import type { PropsWithChildren, FC } from "react"
+import type { ComponentTree } from "component"
 
-type AppContext = {}
+type AppContext = {
+  componentTree: ComponentTree[]
+  categoryGroup?: ComponentTree
+  category?: ComponentTree
+}
 
-const AppContext = createContext<AppContext>({})
+const AppContext = createContext<AppContext>({
+  componentTree: [],
+})
 
-export type AppProviderProps = PropsWithChildren
+export type AppProviderProps = PropsWithChildren<{
+  componentTree: ComponentTree[]
+  categoryGroup?: ComponentTree
+  category?: ComponentTree
+}>
 
-export const AppProvider: FC<AppProviderProps> = ({ children }) => {
-  const value = useMemo(() => ({}), [])
+export const AppProvider: FC<AppProviderProps> = ({
+  componentTree,
+  categoryGroup,
+  category,
+  children,
+}) => {
+  const value = useMemo(
+    () => ({ componentTree, categoryGroup, category }),
+    [componentTree, categoryGroup, category],
+  )
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>
 }
