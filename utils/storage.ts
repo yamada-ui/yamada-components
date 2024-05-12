ab959fccda3924aa8e3b1d85c91ece3d7b6f6ee6
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-12 17:18:52 +0900

    feat(storage): added cookie management functions

---
+++ utils/storage.ts
@@ -0,0 +1,28 @@
+export const getCookie = <T extends any>(
+  cookie: string,
+  key: string,
+  fallback: string = "{}",
+) => {
+  const match = cookie.match(new RegExp(`(^| )${key}=([^;]+)`))
+  const value = match?.[2] ?? fallback
+
+  return (
+    /^\{.*\}$|^\[.*\]$/.test(value) ? JSON.parse(match?.[2] ?? fallback) : value
+  ) as T
+}
+
+export const getCookies = <T extends any>(cookie: string, reg: RegExp) => {
+  const matches = [
+    ...cookie.matchAll(new RegExp(`(?:^| )(${reg.source})=([^;]+)`, "g")),
+  ]
+
+  return matches.map((match) => JSON.parse(match[2])) as T[]
+}
+
+export const setCookie = (key: string, value: string) => {
+  document.cookie = `${key}=${value}; max-age=31536000; path=/`
+}
+
+export const deleteCookie = (key: string) => {
+  document.cookie = `${key}=; max-age=0; path=/`
+}
