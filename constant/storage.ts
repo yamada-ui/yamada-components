495eca768304c708c5451442daf51dea571fab8c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-12 17:19:28 +0900

    feat(constants): added STORAGE to constants and new file

---
+++ constant/storage.ts
@@ -0,0 +1,5 @@
+export const STORAGE = {
+  COMPONENT_LAYOUT: "yamada-component-layout",
+  COMPONENT_CODE_PREVIEW_DIRECTION: "yamada-component-code-preview-direction",
+  COMPONENT_CODE_PREVIEW_IS_OPEN: "yamada-component-code-preview-is-open",
+}
