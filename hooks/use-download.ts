5e0c1d69899759aef494d6deac9e3e7d39298b7c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-12 17:18:34 +0900

    feat(hooks): added useDownload hook for zip downloads

---
+++ hooks/use-download.ts
@@ -0,0 +1,39 @@
+import JSZip from "jszip"
+import { useCallback } from "react"
+
+type UseDownloadProps = {
+  folderName?: string
+  files?: { path: string; data: string }[]
+}
+
+export const useDownload = (props: UseDownloadProps = {}) => {
+  const onDownload = useCallback(
+    async ({ folderName, files }: UseDownloadProps = {}) => {
+      folderName ??= props.folderName
+      files ??= props.files ?? []
+
+      if (!folderName) return
+
+      const zip = new JSZip()
+      const folder = zip.folder(folderName)
+
+      files.forEach(({ path, data }) => folder.file(path, data))
+
+      const content = await zip.generateAsync({ type: "blob" })
+
+      const el = document.createElement("a")
+
+      el.href = URL.createObjectURL(content)
+      el.download = `${folderName}.zip`
+
+      document.body.appendChild(el)
+
+      el.click()
+
+      document.body.removeChild(el)
+    },
+    [props],
+  )
+
+  return { onDownload }
+}
