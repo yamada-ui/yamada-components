d8a8cd153bc40f5d578b16d806d15cb17bcafca2
taroj1205 <taroj1205@gmail.com>
2024-05-31 12:53:59 +1200

    feat(stats): added progress card with color

--- contents/data-display/stats/progress-card/index.tsx
+++ contents/data-display/stats/progress-card/index.tsx
@@ -1,15 +1,17 @@
-import { Card, Progress, Text } from "@yamada-ui/react"
+import { Card, CardBody, Progress, Text } from "@yamada-ui/react"
 
 const ProgressCard = () => {
   return (
-    <Card padding="xl" textAlign="start">
-      <Text fontSize="xs" fontWeight={700} color="gray">
-        MONTHLY GOAL
-      </Text>
-      <Text fontSize="lg" fontWeight={500}>
-        $5.431 / $10.000
-      </Text>
-      <Progress value={54.31} mt="md" size="lg" borderRadius="xl" />
+    <Card textAlign="start" variant="outline" maxW="lg" mx="auto">
+      <CardBody gap="0" p="lg">
+        <Text fontSize="xs" fontWeight={700} color="gray">
+          MONTHLY GOAL
+        </Text>
+        <Text fontSize="lg" fontWeight={500}>
+          $5,431 / $10,000
+        </Text>
+        <Progress value={54.31} mt="md" size="lg" borderRadius="xl" />
+      </CardBody>
     </Card>
   )
 }
