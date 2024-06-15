c90001cba5ca8e0903bd771b162ab7c2f38c7708
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-06-15 17:33:59 +0900

    refactor: optimized icon imports and usage

--- contents/disclosure/tabs/with-icons/index.tsx
+++ contents/disclosure/tabs/with-icons/index.tsx
@@ -1,24 +1,24 @@
-import { Icon, Tab, Tabs } from "@yamada-ui/react"
-import { Building2, CreditCard, UserRound, Users } from "lucide-react"
+import { Building2, CreditCard, UserRound, Users } from "@yamada-ui/lucide"
+import { Tab, Tabs } from "@yamada-ui/react"
 import type { FC } from "react"
 
 const WithIcons: FC = () => {
   return (
     <Tabs>
       <Tab gap="sm">
-        <Icon as={UserRound} />
+        <UserRound />
         My Account
       </Tab>
       <Tab gap="sm">
-        <Icon as={Building2} />
+        <Building2 />
         Company
       </Tab>
       <Tab gap="sm">
-        <Icon as={Users} />
+        <Users />
         Team Members
       </Tab>
       <Tab gap="sm">
-        <Icon as={CreditCard} />
+        <CreditCard />
         Billing
       </Tab>
     </Tabs>
