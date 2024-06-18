57769518328eba20bfb160241fc06d6d9b5720a5
taroj1205 <taroj1205@gmail.com>
2024-06-18 12:57:13 +1200

    fix(comments): made the `comments` examples be more responsive

--- contents/data-display/comments/with-html-content/index.tsx
+++ contents/data-display/comments/with-html-content/index.tsx
@@ -1,31 +1,66 @@
-import { Avatar, Box, Card, CardBody, Text, VStack } from "@yamada-ui/react"
+import {
+  Avatar,
+  Box,
+  Card,
+  CardBody,
+  Grid,
+  GridItem,
+  Text,
+} from "@yamada-ui/react"
 import type { FC } from "react"
 
 export const CommentWithHTMLContent: FC = () => {
   return (
     <Card variant="outline" maxW="md">
-      <CardBody flexDir="row" placeItems="start">
-        <Avatar src="https://avatars.githubusercontent.com/u/61367823?v=4" />
-        <VStack gap="sm">
-          <Box>
+      <CardBody>
+        <Grid
+          templateAreas={`
+            "avatar user empty"
+            "avatar content content"
+          `}
+          gap="sm"
+          _container={[
+            {
+              maxW: "300px",
+              css: {
+                gridTemplateAreas: `
+                  "avatar user empty"
+                  "content content content"
+                `,
+                placeItems: "center",
+              },
+            },
+          ]}
+        >
+          <GridItem
+            area="avatar"
+            pr="sm"
+            _container={[{ maxW: "300px", css: { pr: "xs" } }]}
+          >
+            <Avatar src="https://avatars.githubusercontent.com/u/61367823?v=4" />
+          </GridItem>
+          <GridItem area="user" placeItems="center">
             <Text fontSize="sm">taroj1205</Text>
             <Text color="gray" fontSize="xs">
               10 minutes ago
             </Text>
-          </Box>
-          <Box
-            dangerouslySetInnerHTML={{
-              __html: `<p>I use <a href="https://heroku.com/" rel="noopener noreferrer" target="_blank">Heroku</a> to host my Node.js application, but MongoDB add-on appears to be too <strong>expensive</strong>. I consider switching to <a href="https://www.digitalocean.com/" rel="noopener noreferrer" target="_blank">Digital Ocean</a> VPS to save some cash.</p>`,
-            }}
-            fontSize="sm"
-            css={{
-              "& a": {
-                color: "var(--ui-colors-link)",
-                textDecoration: "underline",
-              },
-            }}
-          />
-        </VStack>
+          </GridItem>
+          <GridItem area="content">
+            <Box
+              dangerouslySetInnerHTML={{
+                __html: `<p>I use <a href="https://heroku.com/" rel="noopener noreferrer" target="_blank">Heroku</a> to host my Node.js application, but MongoDB add-on appears to be too <strong>expensive</strong>. I consider switching to <a href="https://www.digitalocean.com/" rel="noopener noreferrer" target="_blank">Digital Ocean</a> VPS to save some cash.</p>`,
+              }}
+              fontSize="sm"
+              css={{
+                "& a": {
+                  color: "var(--ui-colors-link)",
+                  textDecoration: "underline",
+                },
+              }}
+              onClick={(e) => e.preventDefault()}
+            />
+          </GridItem>
+        </Grid>
       </CardBody>
     </Card>
   )
