f1a1dd12296432b94cdc819cc2c9662732cccbb9
yuto ichinose <yuto.0526.sano@gmail.com>
2024-06-26 23:09:49 +0900

    feat: add `SimpleWithDescription`

---
+++ contents/overlay/menus/simple-with-descriptions/index.tsx
@@ -0,0 +1,65 @@
+import { ChevronDown } from "@yamada-ui/lucide"
+import {
+  Button,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  VStack,
+} from "@yamada-ui/react"
+import type { FC } from "react"
+
+const items = [
+  {
+    name: "Analytics",
+    description: "Get a better understanding of your traffic",
+  },
+  {
+    name: "Engagement",
+    description: "Speak directly to your customers",
+  },
+  {
+    name: "Security",
+    description: "Your customers' data will be safe and secure",
+  },
+  {
+    name: "Integrations",
+    description: "Connect with third-party tools",
+  },
+  {
+    name: "Automations",
+    description: "Build strategic funnels that will convert",
+  },
+]
+
+const SimpleWithDescriptions: FC = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} variant="ghost" rightIcon={<ChevronDown />}>
+        Solutions
+      </MenuButton>
+
+      <MenuList>
+        {items.map((item, i) => (
+          <MenuItem key={i} as={VStack} alignItems="start">
+            <Text
+              as={Link}
+              onClick={(e) => e.preventDefault()}
+              color={["black", "white"]}
+              _hover={{ textDecor: "none" }}
+            >
+              {item.name}
+            </Text>
+            <Text color={["blackAlpha.500", "whiteAlpha.500"]}>
+              {item.description}
+            </Text>
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  )
+}
+
+export default SimpleWithDescriptions
