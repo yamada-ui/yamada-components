2385efee4d6021ffd225f22e761d2516fcaf9e64
yuto ichinose <yuto.0526.sano@gmail.com>
2024-07-06 09:50:23 +0900

    fix: split components

--- contents/overlay/context-menus/mail-box/index.tsx
+++ contents/overlay/context-menus/mail-box/index.tsx
@@ -1,17 +1,6 @@
-import { ArchiveIcon, ArchiveRestoreIcon, TrashIcon } from "@yamada-ui/lucide"
-import {
-  Center,
-  Text,
-  Tabs,
-  Tab,
-  TabPanel,
-  ContextMenu,
-  ContextMenuTrigger,
-  MenuList,
-  MenuItem,
-  HStack,
-} from "@yamada-ui/react"
-import { type Dispatch, type SetStateAction, type FC, useState } from "react"
+import { Center, Text, Tabs, Tab, TabPanel } from "@yamada-ui/react"
+import { type FC, useState } from "react"
+import { MailItem } from "./mail-item"
 
 const mailsData = [
   {
@@ -45,74 +34,6 @@ const mailsData = [
     time: "18:55",
   },
 ]
-
-const MailItem: FC<{
-  id: number
-  currentId: number
-  title: string
-  description: string
-  time: string
-  isArchived?: boolean
-  handleDelete?: () => void
-  handleArchive?: () => void
-  setCurrentId: Dispatch<SetStateAction<number>>
-}> = ({
-  id,
-  currentId,
-  time,
-  description,
-  title,
-  isArchived,
-  handleArchive,
-  handleDelete,
-  setCurrentId,
-}) => {
-  return (
-    <ContextMenu
-      onOpen={() => {
-        setCurrentId(id)
-      }}
-      isOpen={id === currentId}
-    >
-      <ContextMenuTrigger
-        as={Center}
-        w="full"
-        p="md"
-        flexDir="row"
-        justifyContent="space-between"
-        borderBottomWidth="1px"
-        borderBottomStyle="solid"
-        bgColor={id === currentId ? "primary" : undefined}
-      >
-        <HStack gap="md">
-          <Text>{title}</Text>
-          <Text>{description}</Text>
-        </HStack>
-        <Center>
-          <Text>{time}</Text>
-        </Center>
-      </ContextMenuTrigger>
-
-      <MenuList>
-        <MenuItem icon={<TrashIcon color="danger" />} onClick={handleDelete}>
-          Delete
-        </MenuItem>
-        <MenuItem
-          icon={
-            isArchived ? (
-              <ArchiveRestoreIcon color="success" />
-            ) : (
-              <ArchiveIcon color="danger" />
-            )
-          }
-          onClick={handleArchive}
-        >
-          {isArchived ? "Remove Archive" : "Archive"}
-        </MenuItem>
-      </MenuList>
-    </ContextMenu>
-  )
-}
 
 const MailBox: FC = () => {
   const [mainMails, setMainMails] = useState<typeof mailsData>(mailsData)
