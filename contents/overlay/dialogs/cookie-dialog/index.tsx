8e86b98b7c28791b8b77de907ae9d1b721d7c3e7
Haru <23bus1maesub@gmail.com>
2024-07-08 11:28:48 +0900

    refactor: refactor tsx and metadata files

--- contents/overlay/dialogs/cookie-dialog/index.tsx
+++ contents/overlay/dialogs/cookie-dialog/index.tsx
@@ -1,20 +1,11 @@
-import {
-  Button,
-  Center,
-  Dialog,
-  Link,
-  Text,
-  useDisclosure,
-} from "@yamada-ui/react"
+import { Button, Dialog, Link, Text, useDisclosure } from "@yamada-ui/react"
 import type { FC } from "react"
 
 const CookieDialog: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <Center>
-      <Button onClick={onOpen} m="50px">
-        Open Dialog
-      </Button>
+    <>
+      <Button onClick={onOpen}>Open Dialog</Button>
 
       <Dialog
         isOpen={isOpen}
@@ -28,10 +19,12 @@ const CookieDialog: FC = () => {
           We use cookies to enhance your experience on our website. By clicking
           "Accept", you agree to our use of cookies for analytics and
           personalized content. For more information, please read our{" "}
-          <Link href="./dialogs">Cookie Policy.</Link>
+          <Link href="/privacy/cookie" onClick={(e) => e.preventDefault()}>
+            Cookie Policy.
+          </Link>
         </Text>
       </Dialog>
-    </Center>
+    </>
   )
 }
 
