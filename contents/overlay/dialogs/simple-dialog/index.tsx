9e8d902777966a49d320255ccb65fed1a54ea1b6
Haru-0001 <23bus1maesub@gmail.com>
2024-07-13 13:37:01 +0900

    feat: add new dialog example

---
+++ contents/overlay/dialogs/simple-dialog/index.tsx
@@ -0,0 +1,41 @@
+import { Trash2 } from "@yamada-ui/lucide"
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@yamada-ui/react"
+import type { FC } from "react"
+
+const SimpleDialog: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onOpen}>Open Dialog</Button>
+      <Dialog
+        isOpen={isOpen}
+        onClose={onClose}
+        cancel="Cancel"
+        onCancel={onClose}
+        success="Delete"
+        onSuccess={onClose}
+      >
+        <DialogHeader>
+          <VStack alignItems="center">
+            <Trash2 boxSize="80px" color="red" />
+            <Text>Are you sure?</Text>
+          </VStack>
+        </DialogHeader>
+        <Text>
+          This action cannot be undone. Are you sure you want to delete this
+          item?
+        </Text>
+      </Dialog>
+    </>
+  )
+}
+
+export default SimpleDialog
