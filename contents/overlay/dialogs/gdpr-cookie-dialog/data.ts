c84c053a615b5941145f219f5cfd6b846f61ff74
Haru-0001 <23bus1maesub@gmail.com>
2024-07-19 04:27:27 +0900

    feat: add gdpr cookie

---
+++ contents/overlay/dialogs/gdpr-cookie-dialog/data.ts
@@ -0,0 +1,174 @@
+export const CookieData = [
+  {
+    id: 1,
+    title: "Strictly Necessary",
+    description:
+      "These cookies are essential for you to browse the website and use its features, such as accessing secure areas of the site. Cookies that allow web shops to hold your items in your cart while you are shopping online are an example of strictly necessary cookies. These cookies will generally be first-party session cookies. While it is not required to obtain consent for these cookies, what they do and why they are necessary should be explained to the user.",
+    isChecked: true,
+    isDisabled: true,
+    tableData: [
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Genki_Dama",
+        expiration: "1 month",
+        description: "This cookie is used for the Genki Dama.",
+      },
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Kamehameha",
+        expiration: "1 month",
+        description: "This cookie is used for the Kamehameha.",
+      },
+      {
+        provider: "Vegeta",
+        domain: "https://yamada-ui.com/",
+        cookie: "Galick_Gun",
+        expiration: "Session",
+        description: "This cookie is used for the Galick Gun.",
+      },
+      {
+        provider: "Krillin",
+        domain: "https://yamada-ui.com/",
+        cookie: "Destructo_Disk",
+        expiration: "1 year",
+        description: "This cookie is used for the Destructo Disc.",
+      },
+      {
+        provider: "Yamcha",
+        domain: "https://yamada-ui.com/",
+        cookie: "Wolf_Fang_Fist",
+        expiration: "Session",
+        description: "This cookie is used for the Wolf Fang Fist.",
+      },
+      {
+        provider: "Tien_Shinhan",
+        domain: "https://yamada-ui.com/",
+        cookie: "Tri_Beam",
+        expiration: "Session",
+        description: "This cookie is used for the Tri Beam.",
+      },
+      {
+        provider: "Android_17",
+        domain: "https://yamada-ui.com/",
+        cookie: "Android_Barrier",
+        expiration: "Session",
+        description: "This cookie is used for the Android Barrier.",
+      },
+      {
+        provider: "Android_18",
+        domain: "https://yamada-ui.com/",
+        cookie: "Energy_Blast",
+        expiration: "Session",
+        description: "This cookie is used for the Energy Wave.",
+      },
+      {
+        provider: "Piccolo",
+        domain: "https://yamada-ui.com/",
+        cookie: "Special_Beam_Cannon",
+        expiration: "Persistent",
+        description: "This cookie is used for the Special Beam Cannon.",
+      },
+      {
+        provider: "Piccolo",
+        domain: "https://yamada-ui.com/",
+        cookie: "Hellzone_Grenade",
+        expiration: "Session",
+        description: "This cookie is used for the Hellzone Grenade.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Preferences",
+    description:
+      "Also known as “functionality cookies,” these cookies allow a website to remember choices you have made in the past, like what language you prefer, what region you would like weather reports for, or what your user name and password are so you can automatically log in.",
+    tableData: [
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Genki_Dama",
+        expiration: "1 month",
+        description: "This cookie is used for the Genki Dama.",
+      },
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Kamehameha",
+        expiration: "1 month",
+        description: "This cookie is used for the Kamehameha.",
+      },
+      {
+        provider: "Vegeta",
+        domain: "https://yamada-ui.com/",
+        cookie: "Galick_Gun",
+        expiration: "Session",
+        description: "This cookie is used for the Galick Gun.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Statistics",
+    description:
+      "Also known as “performance cookies,” these cookies collect information about how you use a website, like which pages you visited and which links you clicked on. None of this information can be used to identify you. It is all aggregated and, therefore, anonymized. Their sole purpose is to improve website functions. This includes cookies from third-party analytics services as long as the cookies are for the exclusive use of the owner of the website visited.",
+    tableData: [
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Genki_Dama",
+        expiration: "1 month",
+        description: "This cookie is used for the Genki Dama.",
+      },
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Kamehameha",
+        expiration: "1 month",
+        description: "This cookie is used for the Kamehameha.",
+      },
+      {
+        provider: "Vegeta",
+        domain: "https://yamada-ui.com/",
+        cookie: "Galick_Gun",
+        expiration: "Session",
+        description: "This cookie is used for the Galick Gun.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Marketing",
+    description:
+      "These cookies track your online activity to help advertisers deliver more relevant advertising or to limit how many times you see an ad. These cookies can share that information with other organizations or advertisers. These are persistent cookies and almost always of third-party provenance.",
+    tableData: [
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Genki_Dama",
+        expiration: "1 month",
+        description: "This cookie is used for the Genki Dama.",
+      },
+      {
+        provider: "Son_Goku",
+        domain: "https://yamada-ui.com/",
+        cookie: "Kamehameha",
+        expiration: "1 month",
+        description: "This cookie is used for the Kamehameha.",
+      },
+      {
+        provider: "Vegeta",
+        domain: "https://yamada-ui.com/",
+        cookie: "Galick_Gun",
+        expiration: "Session",
+        description: "This cookie is used for the Galick Gun.",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Other",
+    description: "Other Cookies are cookies that do not belong to a category.",
+  },
+]
