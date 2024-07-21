697ba98c7351104d7c69ddaf285867f4123d0d4a
Haru-0001 <23bus1maesub@gmail.com>
2024-07-22 03:33:33 +0900

    feat: add three-buttons-dialog

---
+++ contents/overlay/dialogs/three-buttons-dialog/index.tsx
@@ -0,0 +1,41 @@
+import { CircleHelp } from "@yamada-ui/lucide"
+import {
+  Button,
+  Dialog,
+  DialogFooter,
+  DialogHeader,
+  Spacer,
+  Text,
+  useDisclosure,
+} from "@yamada-ui/react"
+import type { FC } from "react"
+
+const ThreeButtonsDialog: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const name = "Untitled-1"
+  return (
+    <>
+      <Button onClick={onOpen}>Open Dialog</Button>
+      <Dialog isOpen={isOpen} onClose={onClose} blockScrollOnMount={false}>
+        <DialogHeader>
+          <CircleHelp boxSize="50px" color="info" />
+          <Text>Unsaved changes</Text>
+        </DialogHeader>
+        <Text>Do you want to save changes to '{name}' before closing?</Text>
+        <DialogFooter>
+          <Button onClick={onClose} variant="ghost">
+            Cancel
+          </Button>
+          <Spacer />
+          <Button onClick={onClose} variant="ghost" colorScheme="danger">
+            Discard
+          </Button>
+          <Button onClick={onClose} variant="outline" colorScheme="primary">
+            Save
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  )
+}
+export default ThreeButtonsDialog
