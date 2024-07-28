9334add29deb508012e7a4e59f474123287632fa
Haru-0001 <23bus1maesub@gmail.com>
2024-07-28 16:31:35 +0900

    fix: form-drawer

--- contents/overlay/drawers/form-drawer/index.tsx
+++ contents/overlay/drawers/form-drawer/index.tsx
@@ -19,7 +19,12 @@ const FormDrawer: FC = () => {
     <>
       <Button onClick={onOpen}>Open Drawer</Button>
 
-      <Drawer isOpen={isOpen} onClose={onClose} blockScrollOnMount={false}>
+      <Drawer
+        as="form"
+        isOpen={isOpen}
+        onClose={onClose}
+        blockScrollOnMount={false}
+      >
         <DrawerHeader>Contact Us</DrawerHeader>
 
         <DrawerBody>
@@ -35,12 +40,17 @@ const FormDrawer: FC = () => {
             <Input type="email" placeholder="test@example.com" />
           </FormControl>
           <FormControl isRequired label="Message">
-            <Textarea h="180" placeholder="Your message..." />
+            <Textarea h="xs" placeholder="Your message..." />
           </FormControl>
         </DrawerBody>
 
         <DrawerFooter justifyContent="center">
-          <Button w="100%" colorScheme="primary" onClick={onClose}>
+          <Button
+            type="submit"
+            w="full"
+            colorScheme="primary"
+            onClick={onClose}
+          >
             Send message
           </Button>
         </DrawerFooter>
