1eb30a9b833eaf64ad88eb9bc8ddf68e405e1699
bearone236 <kuma_2306@icloud.com>
2024-08-06 23:46:52 +0900

    refactor: breakpoints-page-sections/faq

--- contents/page-sections/faq/with-image/index.tsx
+++ contents/page-sections/faq/with-image/index.tsx
@@ -11,44 +11,15 @@ import FAQ from "./faq.svg"
 const WithImage = () => {
   return (
     <Flex
-      _container={[
-        {
-          maxW: "900px",
-          css: {
-            flexDir: "column",
-          },
-        },
-      ]}
+      flexDir={{ base: "none", md: "column" }}
       gap="lg"
       py="xl"
       px="md"
       alignItems="center"
       justifyContent="center"
     >
-      <Image
-        src={FAQ.src}
-        alt="faq"
-        w="lg"
-        _container={[
-          {
-            maxW: "900px",
-            css: {
-              w: "90%",
-            },
-          },
-        ]}
-      />
-      <VStack
-        maxW="xl"
-        _container={[
-          {
-            maxW: "900px",
-            css: {
-              maxW: "100%",
-            },
-          },
-        ]}
-      >
+      <Image src={FAQ.src} alt="faq" w={{ base: "3xl", md: "full" }} />
+      <VStack maxW={{ base: "full", md: "3xl" }}>
         <Heading pl="md" size="md">
           Frequently Asked Questions
         </Heading>
