37f56f0de3da3659b1732ae65a2c0083a57ac6a5
taroj1205 <taroj1205@gmail.com>
2024-08-08 21:04:31 +1200

    feat(avatar): added animated `avatar` examples

---
+++ contents/data-display/avatars/avatars-with-hover-motion/index.tsx
@@ -0,0 +1,40 @@
+import { Avatar, AvatarGroup, Motion, Tooltip } from "@yamada-ui/react"
+import type { FC } from "react"
+import { users } from "./users"
+
+const AvatarWithHoverFramer: FC = () => {
+  return (
+    <AvatarGroup
+      as={Motion}
+      whileHover={{
+        width: "350px",
+      }}
+      gap="-3"
+      justifyContent="space-between"
+      display="flex"
+    >
+      {users.map((user) => (
+        <Motion
+          key={user.id}
+          position="relative"
+          whileHover={{
+            scale: 1.5,
+            zIndex: 1,
+          }}
+          borderRadius="full"
+          borderWidth="3px"
+          zIndex={0}
+          bg={["white", "black"]}
+          transitionDelay="0s"
+          transitionDuration="fast"
+        >
+          <Tooltip label={user.name} placement="top" flexShrink="0">
+            <Avatar name={user.name} src={user.src} size="lg" />
+          </Tooltip>
+        </Motion>
+      ))}
+    </AvatarGroup>
+  )
+}
+
+export default AvatarWithHoverFramer
---
+++ contents/data-display/avatars/avatars-with-hover/index.tsx
@@ -0,0 +1,51 @@
+import { Avatar, AvatarGroup, Box, Tooltip } from "@yamada-ui/react"
+import type { FC } from "react"
+import { users } from "./users"
+
+const AvatarWithHover: FC = () => {
+  return (
+    <AvatarGroup
+      _hover={{
+        div: {
+          marginInline: "xs",
+          transitionDelay: "0s",
+        },
+      }}
+      gap="-3"
+    >
+      {users.map((user) => (
+        <Box
+          key={user.id}
+          position="relative"
+          borderRadius="full"
+          sx={{
+            borderWidth: "3px",
+            "&:hover": {
+              transform: "scale(1.5)",
+              zIndex: 1,
+            },
+            "&:hover + div": {
+              transform: "scale(1.2)",
+            },
+            "&:has(+ div:hover)": {
+              transform: "scale(1.2)",
+            },
+            "&:not(:hover)": {
+              zIndex: 0,
+            },
+          }}
+          bg={["white", "black"]}
+          transitionDelay="300ms"
+          transitionDuration="slower"
+          transitionProperty="transform, margin-inline, z-index"
+        >
+          <Tooltip label={user.name} placement="top" flexShrink="0">
+            <Avatar name={user.name} src={user.src} size="lg" />
+          </Tooltip>
+        </Box>
+      ))}
+    </AvatarGroup>
+  )
+}
+
+export default AvatarWithHover
