8d7b3a82fd58cbcd0e76f6641ef060c92f1c6ae8
taroj1205 <taroj1205@gmail.com>
2024-08-11 17:07:59 +1200

    feat(contact-us): added `contact us` page section examples

---
+++ contents/page-sections/contact-us/form/index.tsx
@@ -0,0 +1,85 @@
+import { InstagramIcon, TwitterIcon, YoutubeIcon } from "@yamada-ui/lucide"
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  FormControl,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+  Textarea,
+  VStack,
+} from "@yamada-ui/react"
+import type { FC } from "react"
+import { contacts } from "./contacts"
+
+const Form: FC = () => {
+  return (
+    <Container
+      bgGradient="linear(to-r, primary.700, primary.500)"
+      flexDir={{ base: "row", md: "column" }}
+      p="2xl"
+    >
+      <VStack gap="sm" color="white">
+        <Heading as="h2" size="lg">
+          Contact us
+        </Heading>
+        <Text>
+          Leave your email and we will get back to you within 24 hours
+        </Text>
+        <VStack my="md">
+          {contacts.map((contact) => (
+            <HStack key={contact.name}>
+              <contact.icon fontSize="xl" />
+              <VStack gap="0">
+                <Text fontSize="xs">{contact.name}</Text>
+                <Text>{contact.value}</Text>
+              </VStack>
+            </HStack>
+          ))}
+        </VStack>
+        <ButtonGroup>
+          <IconButton
+            icon={<TwitterIcon fontSize="2xl" color="white" />}
+            rounded="full"
+            as="a"
+            href="https://twitter.com/hirotomoyamada"
+            variant="ghost"
+          />
+          <IconButton
+            icon={<YoutubeIcon fontSize="2xl" color="white" />}
+            rounded="full"
+            as="a"
+            href="https://youtube.com/hirotomoyamada"
+            variant="ghost"
+          />
+          <IconButton
+            icon={<InstagramIcon fontSize="2xl" color="white" />}
+            rounded="full"
+            as="a"
+            href="https://instagram.com/hirotomoyamada"
+            variant="ghost"
+          />
+        </ButtonGroup>
+      </VStack>
+      <VStack bg={["white", "black"]} as="form" rounded="md" p="lg">
+        <FormControl label="Email" isRequired>
+          <Input type="email" placeholder="your@email.com" />
+        </FormControl>
+        <FormControl label="Name">
+          <Input type="text" placeholder="Hirotomo Yamada" />
+        </FormControl>
+        <FormControl label="Message" isRequired>
+          <Textarea placeholder="Your message" />
+        </FormControl>
+        <Button colorScheme="primary" type="submit" placeSelf="flex-end">
+          Send message
+        </Button>
+      </VStack>
+    </Container>
+  )
+}
+
+export default Form
---
+++ contents/page-sections/contact-us/touch-form/contacts.ts
@@ -0,0 +1,24 @@
+import { AtSignIcon, PhoneIcon, MapPinIcon, SunIcon } from "@yamada-ui/lucide"
+
+export const contacts = [
+  {
+    name: "Email",
+    value: "hello@yamada-ui.com",
+    icon: AtSignIcon,
+  },
+  {
+    name: "Phone",
+    value: "(123) 456-7890",
+    icon: PhoneIcon,
+  },
+  {
+    name: "Address",
+    value: "Yamada-ui Inc., 123 Main St",
+    icon: MapPinIcon,
+  },
+  {
+    name: "Working hours",
+    value: "8:00 - 23:00",
+    icon: SunIcon,
+  },
+]
