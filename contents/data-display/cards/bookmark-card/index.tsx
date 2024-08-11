30dfc6db2a86cc4806c225182431bcd423b94f2d
taroj1205 <taroj1205@gmail.com>
2024-08-11 22:00:15 +1200

    feat(cards): added `card` component examples

---
+++ contents/data-display/cards/bookmark-card/index.tsx
@@ -0,0 +1,52 @@
+import { BookmarkIcon } from "@yamada-ui/lucide"
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  Text,
+  VStack,
+  Spacer,
+  IconButton,
+  Image,
+} from "@yamada-ui/react"
+import type { FC } from "react"
+
+const BookmarkCard: FC = () => {
+  return (
+    <Card w="md" rounded="xl" variant="outline">
+      <CardHeader>
+        <VStack gap="xs">
+          <Heading as="h2" size="md">
+            Japan
+          </Heading>
+
+          <Text color="muted">Tokyo, Japan</Text>
+        </VStack>
+
+        <IconButton variant="ghost" icon={<BookmarkIcon fontSize="2xl" />} />
+      </CardHeader>
+
+      <CardBody>
+        <Image
+          src="https://images.pexels.com/photos/248195/pexels-photo-248195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Japan"
+          w="full"
+          rounded="md"
+        />
+      </CardBody>
+
+      <CardFooter>
+        <Text>Photo by Pixabay from Pexels</Text>
+
+        <Spacer />
+
+        <Button colorScheme="primary">Explore</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default BookmarkCard
