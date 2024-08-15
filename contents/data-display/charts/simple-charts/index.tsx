f7b10571a203b89af55d0f74af555ca538089b61
108yen <kazuking.1911@gmail.com>
2024-08-15 17:04:54 +0900

    feat: added other simple chart

--- contents/data-display/charts/simple-charts/index.tsx
+++ contents/data-display/charts/simple-charts/index.tsx
@@ -1,28 +1,107 @@
-import { AreaChart } from "@yamada-ui/charts"
+import {
+  AreaChart,
+  BarChart,
+  DonutChart,
+  LineChart,
+  PieChart,
+  RadarChart,
+} from "@yamada-ui/charts"
+import { Grid, GridItem } from "@yamada-ui/react"
 import { useMemo, type FC } from "react"
 
 const SimpleCharts: FC = () => {
-  const data = useMemo(
+  const mappedData = useMemo(
+    () => [10, 20, 40, 20, 60].map((value, index) => ({ value, index })),
+    [],
+  )
+
+  const pieChartData = useMemo(
     () =>
-      [10, 20, 40, 20, 40, 10, 50].map((value, index) => ({ value, index })),
+      [10, 20, 40, 20, 60].map((value, index) => ({
+        name: `${index}`,
+        value,
+        color: `primary.${(index + 1) * 100}`,
+      })),
     [],
   )
 
   return (
-    <>
-      <AreaChart
-        data={data}
-        series={[{ dataKey: "value", color: "primary.500" }]}
-        dataKey="index"
-        curveType="linear"
-        areaProps={{ isAnimationActive: true }}
-        withXAxis={false}
-        withYAxis={false}
-        gridAxis="none"
-        withTooltip={false}
-        withDots={false}
-      />
-    </>
+    <Grid
+      templateAreas={`
+        "areaChart lineChart barChart pieChart donutChart radarChart"
+      `}
+      gap="sm"
+    >
+      <GridItem area="areaChart" boxSize="sm">
+        <AreaChart
+          data={mappedData}
+          series={[{ dataKey: "value", color: "primary.500" }]}
+          dataKey="index"
+          curveType="linear"
+          areaProps={{ isAnimationActive: true }}
+          withXAxis={false}
+          withYAxis={false}
+          gridAxis="none"
+          withTooltip={false}
+          withDots={false}
+        />
+      </GridItem>
+
+      <GridItem area="lineChart" boxSize="sm">
+        <LineChart
+          data={mappedData}
+          series={[{ dataKey: "value", color: "primary.500" }]}
+          dataKey="index"
+          curveType="linear"
+          lineProps={{ isAnimationActive: true }}
+          withXAxis={false}
+          withYAxis={false}
+          gridAxis="none"
+          withTooltip={false}
+          withDots={false}
+        />
+      </GridItem>
+
+      <GridItem area="barChart" boxSize="sm">
+        <BarChart
+          data={mappedData}
+          series={[{ dataKey: "value", color: "primary.500" }]}
+          dataKey="index"
+          barProps={{ isAnimationActive: true }}
+          withXAxis={false}
+          withYAxis={false}
+          gridAxis="none"
+          withTooltip={false}
+        />
+      </GridItem>
+
+      <GridItem area="pieChart" boxSize="sm">
+        <PieChart
+          data={pieChartData}
+          pieProps={{ isAnimationActive: true }}
+          withTooltip={false}
+        />
+      </GridItem>
+
+      <GridItem area="donutChart" boxSize="sm">
+        <DonutChart
+          data={pieChartData}
+          pieProps={{ isAnimationActive: true }}
+          withTooltip={false}
+        />
+      </GridItem>
+
+      <GridItem area="radarChart" boxSize="sm">
+        <RadarChart
+          data={mappedData}
+          series={[{ dataKey: "value", color: "primary.500" }]}
+          dataKey="index"
+          radarProps={{ isAnimationActive: true }}
+          withPolarAngleAxis={false}
+          withTooltip={false}
+        />
+      </GridItem>
+    </Grid>
   )
 }
 
