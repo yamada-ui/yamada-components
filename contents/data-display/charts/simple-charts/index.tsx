073d97c184d33ecd675055836d6161069bad3e09
108yen <kazuking.1911@gmail.com>
2024-08-15 16:33:43 +0900

    feat: added simple area chart

--- contents/data-display/charts/simple-charts/index.tsx
+++ contents/data-display/charts/simple-charts/index.tsx
@@ -1,7 +1,29 @@
-import type { FC } from "react"
+import { AreaChart } from "@yamada-ui/charts"
+import { useMemo, type FC } from "react"
 
 const SimpleCharts: FC = () => {
-  return <></>
+  const data = useMemo(
+    () =>
+      [10, 20, 40, 20, 40, 10, 50].map((value, index) => ({ value, index })),
+    [],
+  )
+
+  return (
+    <>
+      <AreaChart
+        data={data}
+        series={[{ dataKey: "value", color: "primary.500" }]}
+        dataKey="index"
+        curveType="linear"
+        areaProps={{ isAnimationActive: true }}
+        withXAxis={false}
+        withYAxis={false}
+        gridAxis="none"
+        withTooltip={false}
+        withDots={false}
+      />
+    </>
+  )
 }
 
 export default SimpleCharts
