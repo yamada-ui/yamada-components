200c2205355f1ed682ca13c9467ba2ae98a41f69
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-22 10:17:19 +0900

    refactor: use URL object

--- contents/data-display/charts/date-range-temperature-charts/utils.ts
+++ contents/data-display/charts/date-range-temperature-charts/utils.ts
@@ -13,10 +13,15 @@ export const getWeeklyTemperatureData = async (
   startDateString: string,
   endDateString: string,
 ) => {
-  const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=35.6785,34.6937,43.0642,26.2124&longitude=139.6823,135.5023,141.3468,127.6792&hourly=temperature_2m&timezone=Asia%2FTokyo&start_date=${startDateString}&end_date=${endDateString}`,
-    { cache: "force-cache" },
-  )
+  const url = new URL("https://api.open-meteo.com/v1/forecast")
+  url.searchParams.set("latitude", "35.6785,34.6937,43.0642,26.2124")
+  url.searchParams.set("longitude", "139.6823,135.5023,141.3468,127.6792")
+  url.searchParams.set("hourly", "temperature_2m")
+  url.searchParams.set("timezone", "Asia/Tokyo")
+  url.searchParams.set("start_date", startDateString)
+  url.searchParams.set("end_date", endDateString)
+
+  const response = await fetch(url, { cache: "force-cache" })
 
   const data = (await response.json()) as Array<{
     latitude: number
