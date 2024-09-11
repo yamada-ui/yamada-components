728a0d2e928a09784b43b5b6ef2cd5e847298c04
taroj1205 <taroj1205@gmail.com>
2024-09-12 08:01:59 +1200

    refactor(avatars): remove unnecessary line in `AvatarWithText` component

--- contents/data-display/avatars/avatar-with-text/index.tsx
+++ contents/data-display/avatars/avatar-with-text/index.tsx
@@ -8,7 +8,6 @@ const AvatarWithText: FC = () => {
         src="https://avatars.githubusercontent.com/u/109452865?v=4"
         name="Taku"
       />
-
       <VStack gap="0">
         <Text lineClamp={1}>Taku</Text>
         <Text color="muted" lineClamp={1}>
