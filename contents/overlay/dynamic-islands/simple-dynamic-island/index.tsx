9a55bc1f6ff22b151d5d22132733942651611721
yuto ichinose <yuto.0526.sano@gmail.com>
2024-09-27 22:28:25 +0900

    fix: add `px`

--- contents/overlay/dynamic-islands/simple-dynamic-island/index.tsx
+++ contents/overlay/dynamic-islands/simple-dynamic-island/index.tsx
@@ -71,7 +71,7 @@ const DynamicIsland: FC = () => {
           animate={{ opacity: 1, translateY: 0 }}
           transition={{ duration: 0.4, delay: 0.1 }}
         >
-          <Center gap="md">
+          <Center gap="md" px="md">
             <Link
               href="/"
               onClick={(e) => {
