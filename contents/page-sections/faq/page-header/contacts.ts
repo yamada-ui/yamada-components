fd7cb19822698a4b1c894a80811942e055f10621
taroj1205 <taroj1205@gmail.com>
2024-10-21 19:13:45 +1300

    feat(faq): added faq page with page header

---
+++ contents/page-sections/faq/page-header/contacts.ts
@@ -0,0 +1,24 @@
+import { AtSignIcon, MapPinIcon, PhoneIcon, SunIcon } from "@yamada-ui/lucide"
+
+export const contacts = [
+  {
+    name: "Email",
+    icon: AtSignIcon,
+    value: "hello@yamada-ui.com",
+  },
+  {
+    name: "Phone",
+    icon: PhoneIcon,
+    value: "(123) 456-7890",
+  },
+  {
+    name: "Address",
+    icon: MapPinIcon,
+    value: "123 Main St, Tokyo",
+  },
+  {
+    name: "Working hours",
+    icon: SunIcon,
+    value: "8:00 - 23:00",
+  },
+]
